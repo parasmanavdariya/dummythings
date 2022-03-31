978d1bd59a14f81b1956452bfafab061c1ec7fbb
parasmanavdariya <viruspatel0@gmail.com>
2022-03-31 22:49:57 +0530

    basic home page and login design added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import DocumentHead from "../components/DocumentHead";
 import Navbar from '../components/Navbar'
 import Banner from '../components/banner'
+import CTA from "../components/CTA";
+import Feature from "../components/Feature";
 export default function Home() {
   return (
     <div>
       <DocumentHead title={"Home"}/>
       <Navbar/>
       <Banner/>
+      <Feature/>
+      <CTA/>
     </div>
   )
 }
