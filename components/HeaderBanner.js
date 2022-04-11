53c72103d48642dc90ffc84513ca51b1c996c994
parasmanavdariya <viruspatel0@gmail.com>
2022-04-11 13:13:30 +0530

    deployment fix

--- components/HeaderBanner.js
+++ components/HeaderBanner.js
@@ -8,12 +8,12 @@ const HeaderBanner = () => {
                             <span className="flex p-2 rounded-lg bg-indigo-800">
                                 {/* <!-- Heroicon name: outline/speakerphone --> */}
                                 <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                    <path strokeLinecap="round" strokeLinejoin="round" stroke-width="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
                                 </svg>
                             </span>
                             <p className="ml-3 font-medium text-white truncate">
                                 <span className="md:hidden"> We announced a new product! </span>
-                                <span className="hidden md:inline"> Big news! We're excited to announce a brand new product. </span>
+                                <span className="hidden md:inline"> Big news! We&apos;re excited to announce a brand new product. </span>
                             </p>
                         </div>
                         <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
@@ -24,7 +24,7 @@ const HeaderBanner = () => {
                                 <span className="sr-only">Dismiss</span>
                                 {/* <!-- Heroicon name: outline/x --> */}
                                 <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                    <path strokeLinecap="round" strokeLinejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                                 </svg>
                             </button>
                         </div>
