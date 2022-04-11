5fe709916604229e8a25c68f3bb4ddf8fc951d2b
parasmanavdariya <viruspatel0@gmail.com>
2022-04-11 12:37:39 +0530

    user data and header,feature component updated

--- components/Feature.js
+++ components/Feature.js
@@ -3,8 +3,8 @@ return (<>
 <div className="py-12 bg-white">
   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div className="lg:text-center">
-      <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Transactions</h2>
-      <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">A better way to send money</p>
+      <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Totally free data </h2>
+      <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">A better way to get Fake data with out login</p>
       <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.</p>
     </div>
 
@@ -18,7 +18,7 @@ return (<>
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
               </svg>
             </div>
-            <p className="ml-16 text-lg leading-6 font-medium text-gray-900">Competitive exchange rates</p>
+            <p className="ml-16 text-lg leading-6 font-medium text-gray-900">Available for all users</p>
           </dt>
           <dd className="mt-2 ml-16 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
         </div>
@@ -40,11 +40,12 @@ return (<>
           <dt>
             <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
               {/* <!-- Heroicon name: outline/lightning-bolt --> */}
-              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
-              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
+</svg>
             </div>
-            <p className="ml-16 text-lg leading-6 font-medium text-gray-900">Transfers are instant</p>
+            <p className="ml-16 text-lg leading-6 font-medium text-gray-900">Stunning fast data to your request</p>
           </dt>
           <dd className="mt-2 ml-16 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
         </div>
@@ -57,7 +58,7 @@ return (<>
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
               </svg>
             </div>
-            <p className="ml-16 text-lg leading-6 font-medium text-gray-900">Mobile notifications</p>
+            <p className="ml-16 text-lg leading-6 font-medium text-gray-900">Json response to your request</p>
           </dt>
           <dd className="mt-2 ml-16 text-base text-gray-500">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
         </div>
